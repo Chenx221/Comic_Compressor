a3e953b4e78a5f60277fc59c11c761c4154d7a1a
Chenx221 <chenx221@yandex.com>
2024-09-10 15:46:53 +0800

    Use Magick instead of ImageSharp Support more formats and output in original format (2/10)

--- Comic_Compressor/Program.cs
+++ Comic_Compressor/Program.cs
@@ -10,7 +10,7 @@ namespace Comic_Compressor
             Console.OutputEncoding = Encoding.UTF8;
 
             Console.WriteLine("请选择源图像所在位置：");
-            string? sourceImagePath = GetFolderPath();
+            string? sourceImagePath = Utils.GetFolderPath();
             if (string.IsNullOrEmpty(sourceImagePath))
             {
                 Console.WriteLine("未选择文件夹，程序将退出。");
@@ -18,7 +18,7 @@ namespace Comic_Compressor
             }
 
             Console.WriteLine("请选择保存位置：");
-            string? targetStoragePath = GetFolderPath();
+            string? targetStoragePath = Utils.GetFolderPath();
             if (string.IsNullOrEmpty(targetStoragePath))
             {
                 Console.WriteLine("未选择文件夹，程序将退出。");
@@ -29,11 +29,11 @@ namespace Comic_Compressor
             int threadCount = int.Parse(Console.ReadLine() ?? "2");
             Console.WriteLine($"处理线程数设定：{threadCount}");
 
-            Console.WriteLine("目标格式：0 - webp, 1 - avif, 2 - JXL(JPEG-XL), 3 - JPG, 4 - PNG, 5 - 保留原格式(best effort)");
+            Console.WriteLine("目标格式：0 - webp, 1 - avif, 2 - JXL(JPEG-XL), 3 - JPG, 4 - PNG, 5 - BMP, 6 - 保留原格式(best effort)");
             string? modeInput = Console.ReadLine();
             if (modeInput == null)
             {
-                Console.WriteLine("无效输入");
+                Console.WriteLine("无效格式");
                 return;
             }
 
@@ -41,85 +41,28 @@ namespace Comic_Compressor
             {
                 case "0":
                     WebpCompressor.CompressImages(sourceImagePath, targetStoragePath, threadCount);
-                    GetCompressorResult(sourceImagePath, targetStoragePath);
+                    Utils.GetCompressorResult(sourceImagePath, targetStoragePath);
                     break;
                 case "1":
                     AvifCompressor.CompressImages(sourceImagePath, targetStoragePath, threadCount);
-                    GetCompressorResult(sourceImagePath, targetStoragePath);
+                    Utils.GetCompressorResult(sourceImagePath, targetStoragePath);
                     break;
                 case "2":
                     JxlCompressor.CompressImages(sourceImagePath, targetStoragePath, threadCount);
-                    GetCompressorResult(sourceImagePath, targetStoragePath);
+                    Utils.GetCompressorResult(sourceImagePath, targetStoragePath);
                     break;
                 case "3":
                 case "4":
-                    LegacyFormatCompressor.CompressImages(sourceImagePath, targetStoragePath, threadCount,int.Parse(modeInput));
-                    GetCompressorResult(sourceImagePath, targetStoragePath);
-                    break;
                 case "5":
+                    LegacyFormatCompressor.CompressImages(sourceImagePath, targetStoragePath, threadCount,int.Parse(modeInput));
+                    Utils.GetCompressorResult(sourceImagePath, targetStoragePath);
+                    break;
+                case "6":
                     throw new NotImplementedException();
                 default:
                     Console.WriteLine("不支持的格式");
                     break;
             }
         }
-
-
-        private static string? GetFolderPath()
-        {
-            using var dialog = new FolderBrowserDialog();
-            dialog.ShowNewFolderButton = false;
-
-            DialogResult result = dialog.ShowDialog();
-
-            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.SelectedPath))
-            {
-                return dialog.SelectedPath;
-            }
-            else
-            {
-                return null;
-            }
-        }
-
-        private static long GetDirectorySize(string path)
-        {
-            long size = 0;
-
-            foreach (string file in Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories))
-            {
-                FileInfo fileInfo = new(file);
-                size += fileInfo.Length;
-            }
-
-            return size;
-        }
-
-        private static string GetHumanReadableSize(long size)
-        {
-            string[] sizes = { "B", "KB", "MB", "GB", "TB" };
-            double len = size;
-            int order = 0;
-            while (len >= 1024 && order < sizes.Length - 1)
-            {
-                order++;
-                len = len / 1024;
-            }
-
-            return $"{len:0.##} {sizes[order]}";
-        }
-
-
-        private static void GetCompressorResult(string source, string target)
-        {
-            long sourceSize = GetDirectorySize(source);
-            long targetSize = GetDirectorySize(target);
-            double reduced = (sourceSize - targetSize) * 1.0 / sourceSize;
-
-            Console.WriteLine($"压缩前大小：{GetHumanReadableSize(sourceSize)}");
-            Console.WriteLine($"压缩后大小：{GetHumanReadableSize(targetSize)}");
-            Console.WriteLine($"体积已减少{reduced:P}");
-        }
-
     }
 }
---
+++ Comic_Compressor/Utils.cs
@@ -0,0 +1,138 @@
+﻿using ImageMagick;
+using System.Collections.Concurrent;
+
+namespace Comic_Compressor
+{
+    internal class Utils
+    {
+        // Compress an image file
+        // Resize the image if its max dimension is larger than 1200
+        // Set the quality of the compressed image
+        public static void CompressImage(string sourceFilePath, string targetFilePath, MagickFormat mFormat, int quality)
+        {
+            using MagickImage image = new(sourceFilePath);
+
+            uint maxDimension = image.Width > image.Height ? image.Width : image.Height;
+            if (maxDimension > 1200)
+            {
+                double scaleFactor = 1200.0 / maxDimension;
+                uint newWidth = (uint)(image.Width * scaleFactor);
+                uint newHeight = (uint)(image.Height * scaleFactor);
+                image.Resize(newWidth, newHeight);
+            }
+            image.Quality = (uint)quality;
+
+            image.Write(targetFilePath, mFormat);
+        }
+
+        // Get all image files in a directory with supported extensions
+        public static string[] GetImageFiles(string directoryPath)
+        {
+            string[] supportedExtensions = ["*.jpg", "*.jpeg", "*.png", "*.bmp", "*.gif", "*.tiff", "*.jxl", "*.avif", "*.webp"];
+            ConcurrentBag<string> allFiles = [];
+
+            foreach (string extension in supportedExtensions)
+            {
+                foreach (var file in Directory.EnumerateFiles(directoryPath, extension, SearchOption.TopDirectoryOnly))
+                {
+                    allFiles.Add(file);
+                }
+            }
+            return [.. allFiles];
+        }
+
+        // Process all image files in a directory
+        public static void ProcessDirectory(string subdirectory, string sourceImagePath, string targetStoragePath, ShellProgressBar.ProgressBar progressBar, int threadCount, string format, MagickFormat mFormat, int quality)
+        {
+            string relativePath = Path.GetRelativePath(sourceImagePath, subdirectory);
+
+            string targetSubdirectory = Path.Combine(targetStoragePath, relativePath);
+            Directory.CreateDirectory(targetSubdirectory);
+
+            string[] imageFiles = GetImageFiles(subdirectory);
+
+            ParallelOptions options = new()
+            {
+                MaxDegreeOfParallelism = threadCount // Adjust this value to set the number of concurrent threads
+            };
+
+            Parallel.ForEach(imageFiles, options, imageFile =>
+            {
+                string targetFilePath = Path.Combine(targetSubdirectory, Path.GetFileNameWithoutExtension(imageFile) + format);
+
+                if (!File.Exists(targetFilePath))
+                {
+                    CompressImage(imageFile, targetFilePath, mFormat, quality);
+
+                    lock (progressBar)
+                    {
+                        progressBar.Tick($"Processed {Path.GetFileName(imageFile)}");
+                    }
+                }
+                else
+                {
+                    lock (progressBar) { progressBar.Tick($"Skipped {Path.GetFileName(imageFile)}"); }
+                }
+            });
+        }
+
+        // Display the compression result
+        public static void GetCompressorResult(string source, string target)
+        {
+            long sourceSize = GetDirectorySize(source);
+            long targetSize = GetDirectorySize(target);
+            double reduced = (sourceSize - targetSize) * 1.0 / sourceSize;
+
+            Console.WriteLine($"压缩前大小：{GetHumanReadableSize(sourceSize)}");
+            Console.WriteLine($"压缩后大小：{GetHumanReadableSize(targetSize)}");
+            Console.WriteLine($"体积已减少{reduced:P}");
+        }
+
+        // Get the size of a directory
+        public static long GetDirectorySize(string path)
+        {
+            long size = 0;
+
+            foreach (string file in Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories))
+            {
+                FileInfo fileInfo = new(file);
+                size += fileInfo.Length;
+            }
+
+            return size;
+        }
+
+        // Get the path of a folder via a dialog
+        public static string? GetFolderPath()
+        {
+            using var dialog = new FolderBrowserDialog();
+            dialog.ShowNewFolderButton = false;
+
+            DialogResult result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.SelectedPath))
+            {
+                return dialog.SelectedPath;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        // Get the human-readable size of a file
+        public static string GetHumanReadableSize(long size)
+        {
+            string[] sizes = ["B", "KB", "MB", "GB", "TB"];
+            double len = size;
+            int order = 0;
+            while (len >= 1024 && order < sizes.Length - 1)
+            {
+                order++;
+                len /= 1024;
+            }
+
+            return $"{len:0.##} {sizes[order]}";
+        }
+    }
+}
